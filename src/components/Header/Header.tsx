b7cc44b7850e254230697f01ca89ec8d114086d6
minhnhatkool123 <62514136+minhnhatkool123@users.noreply.github.com>
2021-06-21 15:07:21 +0700

    Edit-link-to-manager

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,39 +5,39 @@ import { AppState } from '../../store';
 import './Header.scss';
 
 const Header: React.FC = () => {
-  const loggedIn = useSelector((state: AppState) => state.user.loggedIn);
+	const loggedIn = useSelector((state: AppState) => state.user.loggedIn);
 
-  return (
-    <div className='container container-header'>
-      <header className='header'>
-        <Link to='/'>
-          {/* <img
+	return (
+		<div className='container container-header'>
+			<header className='header'>
+				<Link to='/'>
+					{/* <img
             className='logo'
             src='/resources/images/logo_70.png'
             alt='logo'
           /> */}
-          <span className='logo'>TimTroVN</span>
-        </Link>
-        <div className='left-header'>
-          <div className='slogan'>Website tìm kiếm phòng trọ số 1 Việt Nam</div>
-          {loggedIn ? (
-            <Link to='/quan-ly'>
-              <button className='btn btn-red'>Quản lý tài khoản</button>
-            </Link>
-          ) : (
-            <>
-              <Link to='/dang-ky'>
-                <button className='btn btn-primary'>Đăng ký</button>
-              </Link>
-              <Link to='/dang-nhap'>
-                <button className='btn btn-primary'>Đăng nhập</button>
-              </Link>
-            </>
-          )}
-        </div>
-      </header>
-    </div>
-  );
+					<span className='logo'>TimTroVN</span>
+				</Link>
+				<div className='left-header'>
+					<div className='slogan'>Website tìm kiếm phòng trọ số 1 Việt Nam</div>
+					{loggedIn ? (
+						<Link to='/quan-ly/quan-ly-tin-dang'>
+							<button className='btn btn-red'>Quản lý tài khoản</button>
+						</Link>
+					) : (
+						<>
+							<Link to='/dang-ky'>
+								<button className='btn btn-primary'>Đăng ký</button>
+							</Link>
+							<Link to='/dang-nhap'>
+								<button className='btn btn-primary'>Đăng nhập</button>
+							</Link>
+						</>
+					)}
+				</div>
+			</header>
+		</div>
+	);
 };
 
 export default Header;
